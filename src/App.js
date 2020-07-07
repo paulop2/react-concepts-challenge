93e21922c112aa2f2ce49ea5bfac1c43ad5499fc
paulop2 <paulo225vitor@gmail.com>
2020-07-07 01:46:01 -0300

    React Concepts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from "react";
+import React, {useState, useEffect} from "react"; //ok
+import api from './services/api'; //ok
+
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState ([])
+
+  
+  useEffect(() => {
+    async function loadData() {
+      const response = await api.get('/repositories')
+
+      const loadedRepositories = response.data;
+
+      setRepositories(loadedRepositories);
+    }
+    loadData()
+  },[])
+  
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo projeto ${Date.now()}`,
+      url: "localhost:3333",
+      techs: ["NodeJS", "React"]
+    });
+
+    const repository = response.data;
+    
+
+    setRepositories([...repositories, repository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    /* Funcionou, mas não passou no teste automatizado */
+    // const response = await api.delete(`repositories/${id}`,)
+    // const repository = repositories.splice(id,1)
+    // setRepositories([...repositories, ])
+
+    const response = await api.delete(`repositories/${id}`);
+
+    if (response.status == 204) {
+      const currentRepositories = repositories.filter(repository => repository.id != id);
+
+      setRepositories(currentRepositories)
+    }
+    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id} >
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
